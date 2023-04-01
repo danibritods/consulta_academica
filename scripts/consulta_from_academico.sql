6f50ae5d90d210dd6d3c815894a77a3354c4043b
Daniel Brito <danielbrito@pq.uenf.br>
2023-03-31 21:37:49 -0300

    style: rearrange comments

--- scripts/consulta_from_academico.sql
+++ scripts/consulta_from_academico.sql
@@ -1,7 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS consulta;
 
-CREATE TABLE consulta.aluno AS (
-    SELECT aluno_id, curso_id, matriz_id
     --TODO: situacao (do aluno no curso)
     -- adicionar esse campo ajudaria a filtrar apenas os alunos ativos nos cálculos
     --TODO: data_matricula, ch_acumulada, crd_acumulados, cre
@@ -11,17 +9,20 @@ CREATE TABLE consulta.aluno AS (
     -- as matérias com pessoas quase se formando, critérios técnicos de priorização, etc.
     -- eventualmente permitir calcular quais matérias vão contribuir mais para o avanço e colação de grau dos alunos.
     -- TODO: considerar se area_de_concentracao_id e linha_de_pesquisa_id seriam interessantes nesse contexto.
+CREATE TABLE consulta.aluno AS (
+    SELECT aluno_id, curso_id, matriz_id
     FROM academico.alunos
 );
 
-CREATE TABLE disciplina_matriz AS (
+CREATE TABLE consulta.disciplina_matriz AS (
   SELECT disciplina_id, matriz_id, periodo_referencia, area_de_concentracao_id
   FROM academico.disciplina_matrizes
 );
 
-CREATE TABLE disciplina AS (
+-- TODO: perguntar se é 1 para 1 com sigla
+CREATE TABLE consulta.disciplina AS (
   SELECT
-    disciplina_id,-- TODO: perguntar se é 1 para 1 com sigla
+    disciplina_id,
     sigla,
     nome, 
     laboratorio_id, 
@@ -35,17 +36,17 @@ CREATE TABLE disciplina AS (
   FROM academico.disciplinas 
 );
 
-CREATE TABLE inscricao AS (
+CREATE TABLE consulta.inscricao AS (
   SELECT plano_id, turma_id, situacao, faltas, nota_ef, nota
   FROM academico.inscricoes
 );
 
-CREATE TABLE turma AS (
+CREATE TABLE consulta.turma AS (
   SELECT turma_id, codigo, disciplina_id, ano_semestre 
   FROM academico.tumas
 ); 
 
-CREATE TABLE plano AS (
+CREATE TABLE consulta.plano AS (
   SELECT id_plano, ano_semestre, aluno_id
   FROM academico.planos 
 );
