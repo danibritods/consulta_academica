505012b815a00c05b2c8ab92044115a3913b9ec2
Daniel Brito <danielbrito@pq.uenf.br>
2023-04-14 23:30:02 -0300

    fix: bugs found on first test run

--- scripts/create_demanda.sql
+++ scripts/create_demanda.sql
@@ -1,4 +1,4 @@
-CREATE SCHEMA IF NOT EXISTS demanda
+CREATE SCHEMA IF NOT EXISTS demanda;
 
 CREATE TABLE demanda.contagem_aluno_por_disciplina as (
   SELECT 
@@ -15,17 +15,17 @@ CREATE TABLE demanda.contagem_aluno_por_disciplina as (
 CREATE TABLE demanda.disciplina_aprovada AS (
   SELECT a.aluno_id, m.disciplina_id -- TODO: considerar usar WITH 
   FROM consulta.aluno AS a
-  INNER JOIN consulta.disciplina_matriz AS m ON matriz_id = a.matriz_id
-  INNER JOIN consulta.plano AS p on aluno_id = a.aluno_id 
-  INNER JOIN consulta.inscricao AS i ON plano_id = p.plano_id
-  WHERE situacao == "APR" 
+  INNER JOIN consulta.disciplina_matriz AS m ON m.matriz_id = a.matriz_id
+  INNER JOIN consulta.plano AS p on p.aluno_id = a.aluno_id 
+  INNER JOIN consulta.inscricao AS i ON i.plano_id = p.plano_id
+  WHERE situacao = 'APR'
 );
 
 CREATE TABLE demanda.disciplina_remanescente AS ( --TODO: melhorar o nome
   SELECT a.aluno, m.disciplina_id
   FROM consulta.aluno AS a
-  INNER JOIN consulta.disciplina_matriz AS m ON matriz_id = a.matriz_id
-  JOIN demanda.disciplina_aprovada AS apr ON aluno_id = a.aluno_id
+  INNER JOIN consulta.disciplina_matriz AS m ON m.matriz_id = a.matriz_id
+  JOIN demanda.disciplina_aprovada AS apr ON apr.aluno_id = a.aluno_id
   WHERE m.disciplina_id <> apr.disciplina_id
 );
 
