b3996afa05b7bff6d19a945b1fd0ac827e2a7ed8
Daniel Brito <danielbrito@pq.uenf.br>
2023-04-26 16:16:18 -0300

    feat: add mock data to test

--- scripts/tests/test_scripts.sql
+++ scripts/tests/test_scripts.sql
@@ -1,9 +1,13 @@
 -- This scrips tests the scripts that create `consulta` and `demanda` schemas 
 -- This file is meant to be run by init.sh from the /home/scripts/tests.
 
+DROP DATABASE test_database;
 -- Creating the test database
 CREATE DATABASE test_database TEMPLATE academico_db;
 \c test_database;
+
+-- Inserting the mock data
+\i insert_mock_data.sql
 
 -- Running the scripts that create the `consulta` and `demanda` schemas
 \i ./../consulta_from_academico.sql
@@ -13,4 +17,3 @@ CREATE DATABASE test_database TEMPLATE academico_db;
 
 -- Cleaning the mess
 \c postgres;
-DROP DATABASE test_database;
