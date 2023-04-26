8534f494425f487c6bbb6a4226a9ee0d279299a0
Daniel Brito <danielbrito@pq.uenf.br>
2023-04-26 11:53:18 -0300

    refactor: add name to selected table

--- scripts/demanda_from_consulta.sql
+++ scripts/demanda_from_consulta.sql
@@ -3,7 +3,7 @@ CREATE SCHEMA IF NOT EXISTS demanda;
 --TODO: adicionar equivalências 
 --TODO: adicionar aproveitamento interno
 CREATE TABLE demanda.disciplina_aprovada AS (
-  SELECT a.id, m.disciplina_id -- TODO: considerar usar WITH 
+  SELECT a.id AS aluno_id, m.disciplina_id -- TODO: considerar usar WITH 
   FROM consulta.aluno AS a
   INNER JOIN consulta.plano AS p on p.aluno_id = a.id 
   INNER JOIN consulta.inscricao AS i ON i.plano_id = p.id
@@ -12,10 +12,10 @@ CREATE TABLE demanda.disciplina_aprovada AS (
 );
 
 CREATE TABLE demanda.disciplina_remanescente AS ( --TODO: melhorar o nome
-  SELECT a.id, m.disciplina_id
+  SELECT a.id AS aluno_id, m.disciplina_id
   FROM consulta.aluno AS a
-  INNER JOIN consulta.disciplina_matriz AS m ON m.id = a.matriz_id
   JOIN demanda.disciplina_aprovada AS apr ON apr.aluno_id = a.id
+  INNER JOIN consulta.disciplina_matriz AS m ON m.id = a.matriz_id
   WHERE m.disciplina_id <> apr.disciplina_id
 );
 
