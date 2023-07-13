0258fc01fb9dded83db95aa6dbb6208172d07c14
Daniel Brito <danielbrito@pq.uenf.br>
2023-07-13 15:24:32 -0300

    feat: abstract the complexity of subjects into a single unified table

--- docs/academico_bd.md
+++ docs/academico_bd.md
@@ -1,51 +1,60 @@
 # Academico database notes
 
-## Subjects and its equivalences 
-### Entity relationship model
+## Disciplinas  
+### Modelo Entidade Relacionamento das disciplinas no Acadêmico
 - inscricoes(id, plano_id, turma_id, situacao, faltas, nota_ef, nota) 
   - planos(id, ano_semestre, aluno_id)
   - turmas(id, codigo, disciplina_id, ano_semestre)
 
 - participacoes(id, aluno_id, atividade_id, faltas, nota, satisfatoria_em, insatisfatoria_em)
   - atividades(id, disciplina_id, descricao, ano_semestre)
+- aproveitamentos_de_atividade(id, **aluno_id**, **participacao_id**) #aproveitamento AARE
 
+- equivalencias(equivalida_id, equivalente_id) #equivalida disciplina
 
-- equivalencias(equivalida_id, equivalente_id)
-
-- equivalencias_a_pedido(id, aluno_id, ano_semestre)
+- equivalencias_a_pedido(id, aluno_id, ano_semestre) #aproveitamento disciplina em outra matrícula
   - itens_equivalencia_a_pedido(
     - equivalencia_a_pedido_id,
     - inscricao_id,
     - **disciplina_id**,
     - participacao_id)
 
-- aproveitamentos_internos(id, aluno_id, ano_semestre)
+- aproveitamentos_internos(id, aluno_id, ano_semestre) #aproveitamento disciplina de outra matriz
   - itens_aproveitamento_interno(
     - **aproveitamento_interno_id**,
     - inscricao_id, 
     - **disciplina_id**,
     - participacao_id)
 
-- aproveitamentos_de_atividade(id, **aluno_id**, **participacao_id**)
 
-- isencoes(id, **aluno_id**, **ano_semestre**)
+- isencoes(id, **aluno_id**, **ano_semestre**) #aproveitamento disciplina de outra instituição
+  - itens_isencao(id, **isencao_id**)
   - disciplinas_itens_isencao(id, disciplina_id, item_isencao_id)
-  - itens_isencao(id, isencao_id)
 
-- itens_transferencia_externa(
-  - **transferencia_externa_id**
-  - **nome**
-  - **valor**)
 
-### Rationale 
-- `disciplina_id` is the main object 
+### Modelagem pro consulta_academico
 
-- `inscricoes` is the main course to take a subject
-  - (inscricoes -> turmas -> disciplinas)
-- `participacoes` is the alternative way to take a subject
-  - (participacoes -> atividades -> disciplinas)
-- There are four ways of completing a specific subject without taking it:
-  - `equivalencias_a_pedido`
-  - `aproveitamentos_internos` 
-  - `isencoes`
-  - `transferencia_externa`
+disciplinas_cursadas_aproveitadas_ou_equivalentes(
+    aluno_id, 
+    disciplina_id, 
+    situacao, 
+    nota,  
+    faltas, 
+    ano_semestre,
+    origem : [inscricao, atividade, aprov_isencao, aprov_pedido, aprov_interno, equivalencia]
+)
+
+
+### Explicações 
+- **inscrição**: aluno cursa turma em seu plano de estudos
+- **participação**: aluno cursa AARE, cujo resultado passa por um aproveitamento de atividade para ser contabilizado
+- **isenção**: aproveitamento de estudo realizado em outra instituição, relacionando disciplina externa a disciplina da matriz
+- **equivalência a pedido**: aproveitamento de estudo realizado em outra matrícula na própria UENF, relacionando-o a disciplina da matriz
+- **aproveitamento interno**: aproveitamento de estudo em disciplina no curso atual mas que não seja da matriz, relacionando-o a disciplina da matriz
+
+**Isenção** estabelece disciplina porque não há uma inscrição no banco à qual se referir, uma vez que a disciplina vem de fora e não se relaciona com nada que o aluno tenha já cursado aqui.
+
+As outras se referem a inscrição porque ou se relacionam a inscrição no curso corrente ou em curso anterior.
+
+**Equivalência** já é um conceito diferente, é o estabelecimento de que uma disciplina da UENF equivale a outra. Então todos os conceitos que discutimos acima se aplicam a disciplinas da matriz ou a suas equivalentes. Por exemplo, existe na matriz um Cálculo I mas o aluno cursou uma disciplina mais antiga, Matemática I, que é equivalente. O fato de ter cursado (inscrição/participação) ou aproveitado (via qualquer um dos meios) Matemática I daria ao aluno Cálculo I, caso haja equivalência entre as duas.
+
--- scripts/consulta_academica/consulta_from_academico.sql
+++ scripts/consulta_academica/consulta_from_academico.sql
@@ -45,10 +45,154 @@ CREATE TABLE consulta.participacao AS (
   SELECT id, aluno_id, atividade_id, faltas, nota, satisfatoria_em, insatisfatoria_em
   FROM participacoes 
 );
-
 CREATE TABLE consulta.atividade AS (
   SELECT id, disciplina_id, descricao, ano_semestre
   FROM atividades
+);
+CREATE TABLE consulta.aproveitamento_de_atividade AS (
+  SELECT id, aluno_id, participacao_id
+  FROM aproveitamentos_de_atividade
+);
+
+CREATE TABLE consulta.equivalencia AS (
+  SELECT equivalida_id, equivalente_id
+  FROM equivalencias
+);
+
+CREATE TABLE consulta.disciplina_isencao AS (
+  SELECT i.id, i.aluno_id, dii.disciplina_id, i.ano_semestre
+  FROM isencoes AS i
+  INNER JOIN itens_isencao AS ii
+    ON ii.isencao_id = i.id
+  INNER JOIN disciplinas_itens_isencao AS dii
+    ON dii.item_isencao_id = ii.id
+);
+
+-- TODO: confirmar que apesar da tabela ter relação com inscrição e participação
+--  apenas uma apresentará dados ()
+CREATE TABLE consulta.disciplina_equivalencia_a_pedido AS (
+  SELECT 
+    ep.id,
+    ep.aluno_id, 
+    i_ep.disciplina_id,
+    -- COALESCE(i.situacao) AS situacao,
+    COALESCE(p.nota, i.nota) AS nota,
+    COALESCE(p.faltas, i.faltas) AS faltas,
+    ep.ano_semestre
+  FROM equivalencias_a_pedido AS ep 
+  INNER JOIN itens_equivalencia_a_pedido AS i_ep 
+    ON i_ep.equivalencia_a_pedido_id = ep.id
+  LEFT JOIN consulta.participacao AS p 
+    ON p.id = i_ep.participacao_id 
+  LEFT JOIN consulta.inscricao AS i 
+    ON i.id = i_ep.inscricao_id
+);
+
+CREATE TABLE consulta.disciplina_aproveitamento_interno AS (
+  SELECT 
+    ai.id,
+    ai.aluno_id, 
+    i_ai.disciplina_id,
+    -- COALESCE(i.situacao) AS situacao,
+    COALESCE(p.nota, i.nota) AS nota,
+    COALESCE(p.faltas, i.faltas) AS faltas,
+    ai.ano_semestre
+  FROM aproveitamentos_internos AS ai 
+  INNER JOIN itens_aproveitamento_interno AS i_ai 
+    ON i_ai.aproveitamento_interno_id = ai.id
+  LEFT JOIN consulta.participacao AS p 
+    ON p.id = i_ai.participacao_id 
+  LEFT JOIN consulta.inscricao AS i 
+    ON i.id = i_ai.inscricao_id
+);
+
+CREATE TABLE consulta.disciplina_inscricao AS (
+  SELECT 
+    p.aluno_id, 
+    t.disciplina_id, 
+    i.situacao, 
+    i.nota, 
+    i.nota_ef, 
+    i.faltas, 
+    t.ano_semestre
+  FROM consulta.plano AS p
+  INNER JOIN consulta.inscricao AS i 
+    ON i.plano_id = p.id
+  INNER JOIN consulta.turma AS t 
+    ON t.id = i.turma_id
+);
+
+CREATE TABLE consulta.disciplina_participacao AS (
+  SELECT
+    p.aluno_id,
+    a.disciplina_id,
+    p.nota,
+    p.faltas,
+    a.descricao,
+    a.ano_semestre,
+    CASE
+      WHEN EXISTS (
+        SELECT ap.id
+        FROM consulta.aproveitamento_de_atividade AS ap
+        WHERE ap.aluno_id = p.aluno_id
+          AND ap.participacao_id = p.id
+      ) THEN 'APR'
+      ELSE NULL::VARCHAR(6) 
+    END AS situacao
+  FROM consulta.participacao AS p
+  INNER JOIN consulta.atividade AS a
+    ON p.atividade_id = a.id
+);
+
+
+CREATE TABLE consulta.disciplina_cursada_ou_aproveitada AS (
+  SELECT aluno_id, disciplina_id, situacao, nota, faltas, ano_semestre,
+    'inscricao' AS origem
+  FROM consulta.disciplina_inscricao
+
+  UNION ALL 
+  SELECT aluno_id, disciplina_id, situacao, nota, faltas, ano_semestre,
+    'participacao' AS origem
+  FROM consulta.disciplina_participacao
+
+
+  UNION ALL
+  SELECT aluno_id, disciplina_id, 'APR' AS situacao, nota, faltas, ano_semestre,
+    'aprov_interno' AS origem
+  FROM consulta.disciplina_aproveitamento_interno
+
+  UNION ALL
+  SELECT aluno_id, disciplina_id, 'APR' AS situacao, nota, faltas, ano_semestre,
+    'aprov_pedido' AS origem
+  FROM consulta.disciplina_equivalencia_a_pedido
+
+  UNION ALL
+  SELECT aluno_id, disciplina_id, 'APR' AS situacao, NULL AS nota, NULL AS faltas, ano_semestre,
+    'aprov_isencao' AS origem
+  FROM consulta.disciplina_isencao
+);
+
+CREATE TABLE consulta.disciplina_cursada_aproveitada_ou_equivalente AS (
+  SELECT aluno_id, disciplina_id, situacao, nota, faltas, ano_semestre, origem
+  FROM consulta.disciplina_cursada_ou_aproveitada
+
+  UNION ALL 
+  SELECT 
+    dca.aluno_id, 
+    e.equivalente_id AS disciplina_id,
+    'APR' AS situacao,
+    NULL AS nota,
+    NULL AS faltas, 
+    NULL AS ano_semestre,
+    'equivalencia' AS origem
+  FROM consulta.disciplina_cursada_ou_aproveitada AS dca
+  INNER JOIN consulta.equivalencia AS e
+    ON e.equivalida_id = dca.disciplina_id
+);
+
+CREATE VIEW consulta.disciplina_cursada AS (
+  SELECT *
+  FROM consulta.disciplina_cursada_aproveitada_ou_equivalente
 );
 
 
@@ -62,38 +206,3 @@ CREATE TABLE consulta.co_resuisito AS (
   FROM co_requisitos
 );
 
-
-CREATE TABLE consulta.equivalencia_a_pedido AS (
-  SELECT id, aluno_id, ano_semestre
-  FROM equivalencias_a_pedido
-); 
-
-CREATE TABLE consulta.item_equivalencia_a_pedido AS (
-  SELECT equivalencia_a_pedido_id, inscricao_id, disciplina_id, participacao_id
-  FROM itens_equivalencia_a_pedido
-);
-
-
-
---TODO: adicionar equivalências 
---TODO: adicionar aproveitamento interno
-CREATE TABLE consulta.disciplina_cursada_inscricao AS (
-  SELECT a.id AS aluno_id, t.disciplina_id, i.situacao, i.nota
-  FROM consulta.aluno AS a 
-  INNER JOIN consulta.plano AS p ON p.aluno_id = a.id
-  INNER JOIN consulta.inscricao AS i ON i.plano_id = p.id
-  INNER JOIN consulta.turma AS t ON t.id = i.turma_id
-);
-
-CREATE TABLE consulta.disciplina_cursada_equivalencia_pedido AS (
-  SELECT ep.aluno_id, ep.ano_semestre, i_ep.disciplina_id, 'APR' AS situacao, COALESCE(p.nota, i.nota) AS nota
-  FROM consulta.equivalencia_a_pedido AS ep 
-  INNER JOIN consulta.item_equivalencia_a_pedido AS i_ep ON i_ep.equivalencia_a_pedido_id = ep.id
-  LEFT JOIN consulta.participacao AS p ON p.id = i_ep.participacao_id 
-  LEFT JOIN consulta.inscricao AS i ON i.id = i_ep.inscricao_id
-);
-
-CREATE TABLE consulta.disciplina_cursada AS (
-  SELECT aluno_id, disciplina_id, situacao, nota, 'inscricao' AS origem
-  FROM consulta.disciplina_cursada_inscricao
-);
