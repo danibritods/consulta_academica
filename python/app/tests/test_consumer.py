9f97506e25d47a446f622319cae476241fa22b8e
Daniel Brito <danielbrito@pq.uenf.br>
2023-08-01 20:16:50 -0300

    refactor: add representative string to test case

--- python/app/tests/test_consumer.py
+++ python/app/tests/test_consumer.py
@@ -1,8 +1,8 @@
 from app.data_ingestion import consumer as c
 
-def test_row_to_insert_query():
-    test_body = c.json.loads('{ "table_name":"test", "name":"John", "age":30, "city":"New York"}')
-    expected = ('John', 30, 'New York')
+def test_row_to_insert_query_values():
+    test_body = c.json.loads(b'{ "table_name":"alunos", "id":1, "curso_id":1, "matriz_id":1}')
+    expected = (1,1,1)
     obtained = c._row_to_insert_query(test_body)[1]
 
     assert obtained == expected
