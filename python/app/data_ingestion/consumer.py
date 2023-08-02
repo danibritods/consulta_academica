4c72fe6510510f358b28c4b4d3034c34fc595f82
Daniel Brito <danielbrito@pq.uenf.br>
2023-08-02 16:27:10 -0300

    refactor: use context manager to manage connection to database

--- python/app/data_ingestion/consumer.py
+++ python/app/data_ingestion/consumer.py
@@ -5,7 +5,7 @@ from collections import deque
 import json
 import logging
 
-# Itertools recipe. Used here to implement efficiently apply a function to a list
+# Itertools recipe. Used here to efficiently apply a function to a list
 def consume(iterator, n=None):
     "Advance the iterator n-steps ahead. If n is None, consume entirely."
     # Use functions that consume iterators at C speed.
@@ -25,9 +25,7 @@ def connect_to_db():
         password="auto_academ",
         database="academico_db"
     )
-    cur = conn.cursor()
-    return conn, cur
-
+    return conn
 
 
 def connect_to_queue():
@@ -43,9 +41,7 @@ def connect_to_queue():
 def callback_data_to_db(ch, method, properties, body):
     logging.info(f"callback_data_to_db: message received : {body}")
     message_to_db(body)
-    # rows = cur.fetchall()
-    # for row in rows:
-    #     print(row)
+
 
 def message_to_db(body):
     """
@@ -53,11 +49,9 @@ def message_to_db(body):
                             {"table_name":'table',"id":2,"col1":1,"col2":2,"col3":3}
                             {"table_name":'table',"id":2,"col1":1,"col2":2,"col3":3}]
     """
-    global conn
     
     rows = body_to_rows(body)
     consume(map(_row_to_db,rows))
-    conn.commit()
 
 def body_to_rows(body):
     if b"table_name" not in body:
@@ -75,8 +69,10 @@ def body_to_rows(body):
         
 
 def _row_to_db(row):
-    global cur
-    cur.execute(*_row_to_insert_query(row))
+    with connect_to_db() as conn:
+        with conn.cursor() as cur:
+            cur.execute(*_row_to_insert_query(row))
+            conn.commit()
     #check faster implementations: https://stackoverflow.com/questions/8134602/psycopg2-insert-multiple-rows-with-one-query
 
 def _row_to_insert_query(row):
@@ -91,7 +87,6 @@ def _row_to_insert_query(row):
 
 def main():
     logging.getLogger().setLevel(logging.INFO)
-    conn, cur = connect_to_db()
     channel = connect_to_queue()
     try:
         print(' [*] Waiting for messages. To exit press CTRL+C')
