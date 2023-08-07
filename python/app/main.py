66e10502ad82d9b879b9b93357ffea764fa7410e
Daniel Brito <danielbrito@pq.uenf.br>
2023-08-07 18:09:32 -0300

    feat: implement log file

--- python/app/main.py
+++ python/app/main.py
@@ -2,7 +2,8 @@ import data_ingestion.consumer as consumer
 import logging
 
 def main():
-    logging.getLogger().setLevel(logging.INFO)
+    logging.basicConfig(filename='main.log', encoding='utf-8', level=logging.INFO, format='%(asctime)s %(message)s')
+    logging.info("---------------| running main.py |------------------")
     consumer.consume_queue()
 
 if __name__ == "__main__":
