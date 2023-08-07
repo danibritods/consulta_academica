2bd95e82ecc143e9da402afb7d1b2fc3ec7423e8
Daniel Brito <danielbrito@pq.uenf.br>
2023-08-07 17:44:33 -0300

    refactor: import `sync_academico_db` as `s`instead of `c`for clarity
    It was called `c` because this functionality was in `consumer` module.

--- python/app/tests/test_sync_academico_db.py
+++ python/app/tests/test_sync_academico_db.py
@@ -1,28 +1,28 @@
-from data_ingestion import sync_academico_db as c
+from data_ingestion import sync_academico_db as s
 
 def test_row_to_insert_query_values():
-    test_body = c.json.loads(b'{ "table_name":"alunos", "id":1, "curso_id":1, "matriz_id":1}')
+    test_body = s.json.loads(b'{ "table_name":"alunos", "id":1, "curso_id":1, "matriz_id":1}')
     expected = (1,1,1)
-    obtained = c._row_to_insert_query(test_body)[1]
+    obtained = s._row_to_insert_query(test_body)[1]
 
     assert obtained == expected
 
 def test_row_to_insert_query():
-    test_body = c.json.loads(b'{ "table_name":"alunos", "id":1, "curso_id":1, "matriz_id":1}')
+    test_body = s.json.loads(b'{ "table_name":"alunos", "id":1, "curso_id":1, "matriz_id":1}')
     expected = """
         INSERT INTO alunos (id, curso_id, matriz_id)
         VALUES (%s, %s, %s)
         ON CONFLICT DO NOTHING
         """
 
-    obtained = c._row_to_insert_query(test_body)[0]
+    obtained = s._row_to_insert_query(test_body)[0]
 
     assert obtained == expected
 
 def test_body_to_rows_single_tuple():
     test_body = b'{ "table_name":"alunos", "id":1, "curso_id":1, "matriz_id":1}'
     expected = ({'table_name': 'alunos', 'id': 1, 'curso_id': 1, 'matriz_id': 1},)
-    obtained = c.body_to_rows(test_body)
+    obtained = s.body_to_rows(test_body)
 
     assert obtained == expected
 
@@ -32,7 +32,7 @@ def test_body_to_rows_multiple_tuples():
     
     expected = [{'table_name': 'alunos', 'id': 1, 'curso_id': 1, 'matriz_id': 1},
                 {"table_name":"alunos", "id":2, "curso_id":2, "matriz_id":2}]
-    obtained = c.body_to_rows(test_body)
+    obtained = s.body_to_rows(test_body)
 
     assert obtained == expected
 
@@ -40,7 +40,7 @@ def test_body_to_rows_invalid_body_non_json():
     test_body = b'non json string'
     
     expected = ()
-    obtained = c.body_to_rows(test_body)
+    obtained = s.body_to_rows(test_body)
 
     assert obtained == expected
 
@@ -51,6 +51,6 @@ def test_body_to_rows_invalid_body_json_but_invalid_tuple_format():
     test_body = b'([{"a":1}])'
     
     expected = ()
-    obtained = c.body_to_rows(test_body)
+    obtained = s.body_to_rows(test_body)
 
     assert obtained == expected
