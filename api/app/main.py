f8b908bd7baddffe58cc59741cef17aa0e67e91d
Daniel Brito <danielbrito@pq.uenf.br>
2023-08-09 23:12:35 -0300

    feat(api): implement basic API endpoint and PostgreSQL integration
    This commit introduces the initial implementation of a basic API endpoint and integrates PostgreSQL database connectivity:
    
    - Added 'psycopg2' to the 'requirements.txt' for PostgreSQL adapter.
    - Modified 'main.py' to include necessary imports and define the FastAPI application.
    - Created a database connection configuration using 'psycopg2'.
    - Implemented 'execute_query' function to execute SQL queries and fetch results.
    - Introduced a POST endpoint '/execute_query/' to run arbitrary SQL queries.
    - Added a GET endpoint '/' to provide a simple 'Hello, World' response.
    
    These changes establish the groundwork for querying the 'consulta' schema through the API. The '/execute_query/' endpoint enables users to send SQL queries and receive corresponding results. The '/' endpoint serves as a placeholder for future API enhancements.

--- api/app/main.py
+++ api/app/main.py
@@ -1,15 +1,36 @@
 from typing import Union
 
+import psycopg2
 from fastapi import FastAPI
 
 app = FastAPI()
 
+# Database connection configuration
+db_config = {
+    "dbname": "academico_db",
+    "user": "query_consulta",
+    "password": "query_consulta_password",
+    "host": "db",
+    "port": "5432"
+}
+
+def execute_query(query: str):
+    # Establish a database connection
+    with psycopg2.connect(**db_config) as conn:
+        with conn.cursor() as cur:
+            cur.execute(query)
+            columns = [desc[0] for desc in cur.description]
+            rows = cur.fetchall()
+            return {"columns": columns, "data": rows}
+
+@app.post("/execute_query/")
+def execute_sql_query(query: str):
+    try:
+        result = execute_query(query)
+        return result
+    except Exception as e:
+        return {"error": str(e)}
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
