8cc066e313cd386414a6ebf1b0e2eb14e77038fe
Daniel Brito <danielbrito@pq.uenf.br>
2023-08-25 13:11:44 -0300

    feat(api): add function to get db schema

--- api/app/main.py
+++ api/app/main.py
@@ -33,4 +33,24 @@ def execute_sql_query(query: str):
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+def get_schema():
+    #schema: {schema1: [table1,table2]. schema2: [table1, table2, table3]...}
+    schema = defaultdict(list)
+    for k, v in get_schema_tables()["data"]:
+        schema[k].append(v)
+    return schema
+
+def get_schema_tables():
+    query = """
+    SELECT table_schema, table_name
+    FROM information_schema.tables
+    WHERE table_catalog = current_database()
+    AND table_schema NOT LIKE 'pg_%'
+    AND table_schema <> 'information_schema'
+    AND table_schema <> 'public'
+    AND table_type IN ('BASE TABLE', 'VIEW');
+    """
+
+    #schema_tables: {"columns":["table_schema","table_name"],"data":[["consulta","aluno"],["consulta","disciplina_matriz"],..
+    schema_tables = execute_query(query)
+    return schema_tables
